97f793d3e3d455daf747613aa78958f4a71234ef
joydip62 <joydipbanik6@gmail.com>
2021-10-11 17:25:51 +0530

    make quotes array and also chnage random quote by clicking button

--- js/script.js
+++ js/script.js
@@ -1,19 +1,44 @@
 /******************************************
 project 1 - Random Quote Generator
 ******************************************/
-
+const loadQuote = document.querySelector('#load-quote');
+const quote = document.querySelector('.quote');
 /***
  * `quotes` array
  ***/
 
+var quotes = [
+    `A rose by any other name would smell as sweet.	- William Shakespeare`,
+    `All that glitters is not gold.	-William Shakespeare`,
+    `Ask not what your country can do for you; ask what you can do for your country. -John Kennedy`,
+    `Ask, and it shall be given you; seek, and you shall find.	-the Bible`,
+    `Genius is one percent inspiration and ninety-nine percent perspiration. -Thomas Edison`,
+    `I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character. -Martin Luther King`,
+    `I have always depended on the kindness of strangers. -Blanche Dubois (character)`,
+    `If you want something said, ask a man; if you want something done, ask a woman. -Margaret Thatcher`,
+    `Life is like a box of chocolates. You never know what you’re gonna get. -Forrest Gump (character)`,
+    `Life is like riding a bicycle. To keep your balance, you must keep moving.	-Albert Einstein`
+];
+
 /***
  * `getRandomQuote` function
  ***/
+function getRandomQuote(){
+    var randomNum = Math.floor(Math.random() * quotes.length);
+    // console.log(randomNum);
+    quote.innerText = quotes[randomNum];
+    
+}
 
 /***
  * `printQuote` function
  ***/
 
+
 /***
  * click event listener for the print quote button
  ***/
+ loadQuote.addEventListener('click', function(event){
+    event.preventDefault();
+    getRandomQuote();
+});
